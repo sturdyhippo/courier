034eea243466fd8e088ecfa0ce76066d57283fbd
Landon Smith <l@sturdyhippo.com>
2023-01-18 05:38:04 -0700

    Implement delete in editor. Add roadmap to readme

--- src/panel/editor.rs
+++ src/panel/editor.rs
@@ -2,11 +2,16 @@ use crossterm::event::{Event, KeyCode};
 use tui::backend::Backend;
 use tui::layout::Rect;
 use tui::style::Style;
-use tui::text::{Span, Spans, Text};
+use tui::text::{Span, Text};
 use tui::widgets::Paragraph;
 use tui::Frame;
 use unicode_segmentation::UnicodeSegmentation;
 
+// EditorPartial provides utilities for rendering and controlling a text editor with tui-rs.
+//
+// TODO: It should supports UTF-8 rendering, displaying whitespace symbols, displaying and editing
+// arbitrary codepoints as hex values (including zero-width characters), and fully customizable
+// keybinds with support for vim-style modes and chords.
 pub struct EditorPartial<'a> {
     text: Text<'a>,
     pub has_focus: bool,
@@ -24,73 +29,122 @@ impl<'a> EditorPartial<'a> {
         }
     }
 
-    pub fn draw<B: Backend>(&mut self, f: &mut Frame<B>, r: Rect) {
+    // Draw the editor.
+    pub fn draw<B: Backend>(&self, f: &mut Frame<B>, r: Rect) {
         f.set_cursor(r.x + self.cursor.0 as u16, r.y + self.cursor.1 as u16);
         let paragraph = Paragraph::new(self.text.clone());
         f.render_widget(paragraph, r);
     }
 
+    // Process an event.
     pub fn event(&mut self, event: Event) {
         let Event::Key(key) = event else {
             return;
         };
         match key.code {
-            KeyCode::Up => self.cursor_up(),
-            KeyCode::Down => self.cursor_down(),
-            KeyCode::Left => self.cursor_left(),
-            KeyCode::Right => self.cursor_right(),
-
-            KeyCode::Enter => {
-                let old = &mut self.text.lines[self.cursor.1].0;
-                let mut new: Vec<Span> = Vec::new();
-                // Split the line after the cursor. We also take the entire span where the cursor
-                // is even if part of it is before the cursor so we can have ownership to truncate
-                // it, and then we add it back again.
-                let mut remainder = old.drain(self.cursor_index.0..);
-                let (pre, post) = match remainder.next().unwrap_or(Span::raw("")).content {
-                    std::borrow::Cow::Owned(mut s) => {
-                        let post = std::borrow::Cow::Owned(
-                            s.drain(self.cursor_index.1..).collect::<String>(),
-                        );
-                        let pre = Span::raw(s);
-                        (pre, post)
-                    }
-                    std::borrow::Cow::Borrowed(s) => (
-                        Span::raw(&s[..self.cursor_index.1]),
-                        std::borrow::Cow::Borrowed(&s[self.cursor_index.1..]),
-                    ),
-                };
-                // Add the portion of the span after the cursor to the new line.
-                new.push(Span::raw(post));
-                // Move all remaining spans to the new line.
-                new.extend(&mut remainder);
-                // Move the beginning of the split span back if non-empty.
-                drop(remainder);
-                old.push(pre);
-                // Move the cursor to the start of the new line.
-                self.cursor = (0, self.cursor.1 + 1);
-                self.cursor_index = (0, 0);
-                // Add the new line to the editor.
-                self.text.lines.insert(self.cursor.1, new.into());
+            KeyCode::Up => {
+                self.cursor_up();
             }
-            KeyCode::Delete => {}
+            KeyCode::Down => {
+                self.cursor_down();
+            }
+            KeyCode::Left => {
+                self.cursor_left();
+            }
+            KeyCode::Right => {
+                self.cursor_right();
+            }
+
+            KeyCode::Enter => self.newline(),
+            // TODO: what's the expected behavior for multi-codepoint graphemes?
+            KeyCode::Delete => self.delete(),
+            KeyCode::Backspace => {
+                if self.cursor_left() {
+                    self.delete();
+                }
+            }
             KeyCode::Char(c) => {
                 self.text.lines[self.cursor.1].0[self.cursor_index.0]
                     .content
                     .to_mut()
                     .insert(self.cursor_index.1, c);
-                self.cursor_right()
+                // TODO: we shouldn't move the cursor if inserting a codepoint which combines with
+                // the previous grapheme.
+                self.cursor_right();
             }
 
             _ => {}
         }
     }
 
-    pub fn cursor_right(&mut self) {
-        let line = &self.text.lines[self.cursor.1];
-        if self.cursor.0 < line.width() {
-            self.cursor.0 += 1;
+    // Insert a newline at the cursor location.
+    pub fn newline(&mut self) {
+        let old = &mut self.text.lines[self.cursor.1].0;
+        let mut new: Vec<Span> = Vec::new();
+        // Split the line after the cursor. We also take the entire span where the cursor
+        // is even if part of it is before the cursor so we can have ownership to truncate
+        // it, and then we add it back again.
+        let mut remainder = old.drain(self.cursor_index.0..);
+        let (pre, post) = match remainder.next().unwrap_or(Span::raw("")).content {
+            std::borrow::Cow::Owned(mut s) => {
+                let post =
+                    std::borrow::Cow::Owned(s.drain(self.cursor_index.1..).collect::<String>());
+                let pre = Span::raw(s);
+                (pre, post)
+            }
+            std::borrow::Cow::Borrowed(s) => (
+                Span::raw(&s[..self.cursor_index.1]),
+                std::borrow::Cow::Borrowed(&s[self.cursor_index.1..]),
+            ),
+        };
+        // Add the portion of the span after the cursor to the new line.
+        new.push(Span::raw(post));
+        // Move all remaining spans to the new line.
+        new.extend(&mut remainder);
+        // Move the beginning of the split span back if non-empty.
+        drop(remainder);
+        old.push(pre);
+        // Move the cursor to the start of the new line.
+        self.cursor = (0, self.cursor.1 + 1);
+        self.cursor_index = (0, 0);
+        // Add the new line to the editor.
+        self.text.lines.insert(self.cursor.1, new.into());
+    }
+
+    // Delete the grapheme after the cursor.
+    pub fn delete(&mut self) {
+        let line = &mut self.text.lines[self.cursor.1].0;
+        if let Some((i, grapheme)) = line[self.cursor_index.0]
+            .content
+            .grapheme_indices(true)
+            .nth(self.cursor_index.1)
+        {
+            let remove_range = i..i + grapheme.len();
+            let mut span = std::mem::replace(&mut line[self.cursor_index.0].content, "".into());
+            let mut raw_str = span.to_mut().to_owned().into_bytes();
+            raw_str.drain(remove_range);
+            line[self.cursor_index.0].content = String::from_utf8(raw_str).unwrap().into();
+        } else {
+            let i = self.cursor_index.0 + 1;
+            while i < line.len() {
+                if !line[i].content.is_empty() {
+                    line[i].content.to_mut().remove(0);
+                }
+                if line[i].content.is_empty() {
+                    line.remove(i);
+                }
+            }
         }
+    }
+
+    // Move the cursor right unless it's already at the end of the current line. Returns whether
+    // the cursor was able to move.
+    pub fn cursor_right(&mut self) -> bool {
+        let line = &self.text.lines[self.cursor.1];
+        if self.cursor.0 == line.width() {
+            return false;
+        }
+        self.cursor.0 += 1;
 
         // Decide if we should increment within the span or move to the next one.
         if let Some(_) = line.0[self.cursor_index.0]
@@ -109,12 +163,16 @@ impl<'a> EditorPartial<'a> {
             }
             self.cursor_index.1 = 0;
         }
+        true
     }
 
-    pub fn cursor_left(&mut self) {
-        if self.cursor.0 > 0 {
-            self.cursor.0 -= 1;
+    // Move the cursor left unless it's already at the beginning of the current line. Returns
+    // whether the cursor was able to move.
+    pub fn cursor_left(&mut self) -> bool {
+        if self.cursor.0 == 0 {
+            return false;
         }
+        self.cursor.0 -= 1;
 
         let line = &mut self.text.lines[self.cursor.1];
         // Decide if we should decrement within the span or move to the previous one.
@@ -128,11 +186,14 @@ impl<'a> EditorPartial<'a> {
             }
             self.cursor_index.1 = line.0[self.cursor_index.0].width() - 1;
         }
+        true
     }
 
-    pub fn cursor_down(&mut self) {
+    // Move the cursor down unless it's already at the bottom of the document. Returns whether the
+    // cursor was able to move.
+    pub fn cursor_down(&mut self) -> bool {
         if self.cursor.1 + 1 >= self.text.height() {
-            return;
+            return false;
         }
         self.cursor.1 += 1;
         let line = &self.text.lines[self.cursor.1];
@@ -146,11 +207,14 @@ impl<'a> EditorPartial<'a> {
         } else {
             self.reindex_cursor();
         }
+        true
     }
 
-    pub fn cursor_up(&mut self) {
+    // Move the cursor up unless it's already at the top of the document. Returns whether the
+    // cursor was able to move.
+    pub fn cursor_up(&mut self) -> bool {
         if self.cursor.1 == 0 {
-            return;
+            return false;
         }
         self.cursor.1 -= 1;
         let line = &self.text.lines[self.cursor.1];
@@ -164,8 +228,11 @@ impl<'a> EditorPartial<'a> {
         } else {
             self.reindex_cursor();
         }
+        true
     }
 
+    // Set self.cursor_index based by reading self.cursor and searching the line's graphemes for
+    // the cursor position.
     fn reindex_cursor(&mut self) {
         let line = &self.text.lines[self.cursor.1];
         // Find the span which contains the cursor.
